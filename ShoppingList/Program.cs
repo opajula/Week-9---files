6292807af3b90065ed376f86b3fb521e2fd93018
Olivia <oliviapajula@gmail.com>
2021-12-05 22:46:10 +0200

    add new projects

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fileLocation = @$"C:\Users\Olivia\Samples\shopping";
+            string fileName = @"\\shoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> shoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add an item to your list? Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter an item:");
+                    string newItem = Console.ReadLine();
+                    shoppingList.Add(newItem);
+                }
+                else
+                {
+                    Console.WriteLine("Take care!");
+                    loopActive = false;
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string item in shoppingList)
+            {
+                Console.WriteLine($"You have in your list: {item}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", shoppingList);
+
+
+
+        }
+
+        /*public static string ItemsToShop()
+        {
+            List<string> shoppingList = new List<string>();
+
+            shoppingList.Add("new jeans");
+            shoppingList.Add("a red T-shirt");
+            shoppingList.Add("winter boots for hiking");
+            shoppingList.Add("a cosy sweater");
+            shoppingList.Add("a pair of socks for my partner");
+            shoppingList.Add("automatic feeder for my cat");
+
+            Console.WriteLine(shoppingList.Count + " items in list:");
+
+            foreach (string item in shoppingList)
+            {
+                Console.WriteLine(item);
+            }*/
+        }
+    }
---
+++ WishList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myWishList = new List<string>();
+
+            Console.WriteLine(myWishList.Count); //näitab elementide arvu ehk listi pikkust
+
+            myWishList.Add("coffee machine");
+            myWishList.Add("a new pair of socks");
+            myWishList.Add("box of chocolates");
+            myWishList.Add("new pillow"); //listi sisu
+
+            Console.WriteLine(myWishList.Count);
+
+            foreach(string wish in myWishList)
+            {
+                Console.WriteLine(wish); //hea kuvada iga elementi, mis on listi lisatud
+            }
+        }
+    }
+}
